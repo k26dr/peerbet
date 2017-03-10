24d008ab3eec9e2368b9f69243843e4f1d615f1a
Kedar Iyer <kedarmail@gmail.com>
2017-03-10 18:28:25 -0500

    bid matching

--- sportsbet.sol
+++ sportsbet.sol
@@ -36,8 +36,41 @@ contract SportsBet is owned, mortal {
     Game[] public games;
     mapping(address => uint) private balances;
 
-    function bid(bytes32 game_id, bool home, int64 line) payable {
-        // Get game from list
+    function bidSpread(bytes32 game_id, bool home, int64 line) payable {
+        Game game = getGameById(game_id);
+        Book book = game.books[BetType.Spread];
+        Bid bid = Bid(msg.sender, msg.value, home, line);
+        Bid[] bidStack = home ?  book.awayBids : book.homeBids;
+
+        // Match existing bets (taker)
+        for (uint i = bidStack.length - 1; 
+            -bidStack[i].line >= bid.line && bid.amount > 0 && i >= 0;
+            i--)
+        {
+            address homeAddress = home ? bid.bidder : bidStack[i].bidder;
+            address awayAddress = home ? bidStack[i].bidder : bid.bidder;
+            uint betAmount = bid.amount < bidStack[i].amount ? bid.amount : bidStack[i].amount;
+            int64 betLine = home ? -bidStack[i].line : bidStack[i].line;
+            delete bidStack[i];
+            Bet bet = Bet(homeAddress, awayAddress, betAmount, betLine);
+            book.bets.push(bet);
+            bid.amount -= betAmount;
+        }
+
+        // Use leftover funds to place open bids (maker)
+        addBidToStack(bid, bidStack);
+
+    }
+
+    function addBidToStack(Bid bid, Bid[] stack) {
+        uint i = stack.length - 1;
+        while (stack[i].amount <= bid.amount && i > 0) {
+            i--;
+        }
+        stack
+    }
+
+    function getGameById(bytes32 game_id) private returns (Game g) {
         bool game_exists = false;
         for (uint i = 0; i < games.length; i++) {
             if (games[i].id == game_id) {
@@ -48,21 +81,11 @@ contract SportsBet is owned, mortal {
         }
         if (!game_exists)
             throw;
-
-        // Match bids from opposing stack
-        Bid bid = Bid(msg.sender, msg.value, home, line)
-        if (home)
-            Bid[] bidStack = game.awayBids;
-        else
-            Bid[] bidStack = game.homeBids;
-
-        uint i = bidStack.length - 1;
-        while (true) {
-            break; 
-        }
-
+        return game;
     }
 
+
+    // Returns nothing right now
     function withdraw() returns bool {
         var balance = balances[msg.sender]
         balances[msg.sender] = 0

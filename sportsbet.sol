d30b1393bb18fc63bce437d0a2a1d197cc2335ae
Kedar Iyer <kedarmail@gmail.com>
2017-03-13 15:22:19 -0400

    compiling contract

--- sportsbet.sol
+++ sportsbet.sol
@@ -1,4 +1,6 @@
-contract SportsBet is owned, mortal {
+pragma solidity ^0.4.9;
+
+contract SportsBet {
     enum GameStatus { Open, Locked }
     enum BookType { Spread, MoneyLine, OverUnder }
 
@@ -17,7 +19,7 @@ contract SportsBet is owned, mortal {
     }
 
     struct Book {
-        BookType type;
+        BookType bookType;
         Bid[] homeBids;
         Bid[] awayBids;
         Bet[] bets;
@@ -33,31 +35,45 @@ contract SportsBet is owned, mortal {
         Book[] books;
     }
 
+    address owner;
     Game[] public games;
     mapping(address => uint) private balances;
 
-    function SportsBet() returns (bool success) {
-        return true;
+    function SportsBet() {
+        address owner = msg.sender;
     }
 
     function bidSpread(bytes32 game_id, bool home, int64 line) payable {
         Game game = getGameById(game_id);
-        Book book = game.books[BetType.Spread];
-        Bid bid = Bid(msg.sender, msg.value, home, line);
+        Book book = game.books[uint(BookType.Spread)];
+        Bid memory bid = Bid(msg.sender, msg.value, home, line);
         Bid[] matchStack = home ?  book.awayBids : book.homeBids;
         Bid[] bidStack = home ? book.homeBids : book.awayBids;
 
         // Match existing bets (taker)
-        Bid remainingBid = matchBidToStack(bid, matchStack, home);
+        for (uint i = matchStack.length - 1; 
+            -matchStack[i].line >= bid.line && bid.amount > 0 && i >= 0;
+            i--)
+        {
+            address homeAddress = home ? bid.bidder : matchStack[i].bidder;
+            address awayAddress = home ? matchStack[i].bidder : bid.bidder;
+            uint betAmount = bid.amount < matchStack[i].amount ? bid.amount : matchStack[i].amount;
+            int64 betLine = home ? -matchStack[i].line : matchStack[i].line;
+            delete matchStack[i];
+            Bet memory bet = Bet(homeAddress, awayAddress, betAmount, betLine);
+            book.bets.push(bet);
+            bid.amount -= betAmount;
+        }
+
 
         // Use leftover funds to place open bids (maker)
-        addBidToStack(remainingBid, bidStack);
+        addBidToStack(bid, bidStack);
 
     }
 
-    function cancelBid(address bidder, bytes32 game_id, uint64 line, bool home) returns (bool success) {
+    function cancelBid(address bidder, bytes32 game_id, int64 line, bool home) returns (bool) {
         Game game = getGameById(game_id);
-        Book book = game.books[BetType.Spread];
+        Book book = game.books[uint(BookType.Spread)];
         Bid[] stack = home ? book.homeBids : book.awayBids;
         bool found = true;
         uint i = 0;
@@ -86,27 +102,13 @@ contract SportsBet is owned, mortal {
 
     }
 
-    function matchBidToStack(Bid bid, Bid[] stack, home) private {
-        for (uint i = stack.length - 1; 
-            -stack[i].line >= bid.line && bid.amount > 0 && i >= 0;
-            i--)
-        {
-            address homeAddress = home ? bid.bidder : stack[i].bidder;
-            address awayAddress = home ? stack[i].bidder : bid.bidder;
-            uint betAmount = bid.amount < stack[i].amount ? bid.amount : stack[i].amount;
-            int64 betLine = home ? -stack[i].line : stack[i].line;
-            delete stack[i];
-            Bet bet = Bet(homeAddress, awayAddress, betAmount, betLine);
-            book.bets.push(bet);
-            bid.amount -= betAmount;
-        }
-
-        return bid;
+    function kill () {
+        if (msg.sender == owner) selfdestruct(owner);
     }
 
-    function addBidToStack(Bid bid, Bid[] stack) private {
+    function addBidToStack(Bid bid, Bid[] storage stack) private {
         uint i = stack.length - 1;
-        stack.push(bid) # just to make the stack one item larger
+        stack.push(bid); // just to make the stack one item larger
         while (stack[i].amount <= bid.amount && i > 0) {
             stack[i+1] = stack[i];
             i--;
@@ -114,7 +116,7 @@ contract SportsBet is owned, mortal {
         stack[i+1] = bid;
     }
 
-    function getGameById(bytes32 game_id) private returns (Game g) {
+    function getGameById(bytes32 game_id) private returns (Game storage) {
         bool game_exists = false;
         for (uint i = 0; i < games.length; i++) {
             if (games[i].id == game_id) {
@@ -129,13 +131,12 @@ contract SportsBet is owned, mortal {
     }
 
 
-    // Returns nothing right now
-    function withdraw() returns bool {
-        var balance = balances[msg.sender]
-        balances[msg.sender] = 0
+    function withdraw() returns (bool) {
+        var balance = balances[msg.sender];
+        balances[msg.sender] = 0;
         if (!msg.sender.send(balance)) {
             balances[msg.sender] = balance;
-            throw;
+            return false;
         }
         return true;
     }

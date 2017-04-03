5e0046ce19adc3b19cf098a29a2f27c31c29ad81
Kedar Iyer <kedarmail@gmail.com>
2017-04-03 19:50:20 -0400

    front end game show logic

--- main.js
+++ main.js
@@ -10,23 +10,64 @@ $(docReady.resolve);
 var abiPromise = $.get("abi.json");
 var contractAddressPromise = $.get("contract_address");
 var dictionaryPromise = $.get("data_dictionary.json");
+var contract;
+var dictionary;
 
 $.when(contractAddressPromise, abiPromise, dictionaryPromise, docReady).always(function (contractAddress, abiJSON, dictionary) {
     var contractAddress = contractAddress[0];
     var abi = abiJSON[0];
-    var dictionary = dictionary[0];
-    contract = web3.eth.contract(abi).at(contractAddress)
+    window.dictionary = dictionary[0];
+    contract = web3.eth.contract(abi).at(contractAddress);
 
-    logs = contract.GameCreated({}, { fromBlock: 1, toBlock: 'latest' });
-    logs.get(function (err, logs) {
-        console.log(logs);
-        logs.forEach(log => addGameFromLog(log, dictionary.categories))
-    });
+    routeFromURL();
 
 });
 
-function addGameFromLog (log, categories) {
-    var game = log.args;
+function routeFromURL() {
+    var parts = window.location.hash.slice(1).split('_');
+    route(parts[0], parts.slice(1))
+}
+window.addEventListener("hashchange", routeFromURL);
+
+function route(page, params) {
+    $('.page').hide();
+    switch (page) {
+        case 'spread':
+            spreadShow(params[0]);
+            $("#spread").show();
+            break;
+        case 'games':
+            $("#games").show();
+            gamesList();
+            break;
+        default:
+            gamesList();
+            $("#games").show();
+    }
+}
+
+function getGames () {
+    return new Promise(function (resolve, reject) {
+        if (getGames.prototype.games)
+            resolve(getGames.prototype.games);
+        else {
+            contract.GameCreated({}, { fromBlock: 1 })
+                .get(function (err, logs) {
+                    getGames.prototype.games = logs.map(log => log.args);
+                    resolve(getGames.prototype.games);
+                });
+        }
+    });
+}
+
+function gamesList () {
+    getGames().then(function (games) {
+        games.forEach(game => addGameFromLog(game, dictionary.categories));
+    });
+}
+
+    
+function addGameFromLog (game, categories) {
     var category = categories[parseInt(game.category)];
     var gametime = new Date(parseInt(game.locktime) * 1000);
     var date = gametime.toISOString().slice(0,10);
@@ -38,11 +79,62 @@ function addGameFromLog (log, categories) {
         <td>${category}</td>
         <td>${date}</td>
         <td>${time}</td>
+        <td><a href="#spread_${game.id}">Spread</a></td>
     </tr>`
     $("#games-table").append(row);
 }
-    
-function route(page, params) {
-    $('.page').hide();
-    $('#' + page).show();
+
+
+function spreadShow(id) {
+    getGames().then(function (games) {
+        var game = games.filter(g => g.id == id)[0];
+        var category = dictionary.categories[game.category];
+        $('.matchup').html(`${category} - ${game.home} vs ${game.away}`)
+    });
+    contract.BetPlaced({ game_id: id }, { fromBlock: 1 })
+        .get(function (err, logs) {
+            logs.forEach(log => addBetFromLog(log.args));
+        });
+    var hex = contract.getOpenBids(id);
+    var bids = parseBids(hex);
+    bids.forEach(bid => {
+        if (bid.home) addBidToTable("#home-bids-table", bid);
+        else addBidToTable("#away-bids-table", bid);
+    });
+}
+
+function addBidToTable (table, bid) {
+    var row = `<tr class="bid">
+        <td>${bid.bidder}</td>
+        <td>${bid.amount}</td>
+        <td>${bid.line}</td>
+    </tr>`;
+    $(table).append(row);
+}
+
+function parseBid(hex) {
+    return {
+        bidder: '0x' + hex.slice(0,40),
+        amount: parseInt(hex.slice(40,104), 16),
+        home: parseInt(hex.slice(104,106)) == 1,
+        line: ~~parseInt(hex.slice(106), 16)
+    }
+}
+
+function parseBids(hex) {
+    var bids = []
+    if (hex.slice(0,2) == '0x')
+        hex = hex.slice(2);
+    for (var i=0; i < hex. length; i += 114)
+        bids.push(parseBid(hex.substring(i, i+114)));
+
+    return bids.filter(bid => bid.amount > 0);
+}
+
+function addBetFromLog(bet) {
+    var row = `<tr class="bet">
+        <td>${bet.line}</td>
+        <td>${bet.amount}</td>
+    </tr>`
+    $("#bets-table").append(row);
 }
--- sportsbet.sol
+++ sportsbet.sol
@@ -6,7 +6,7 @@ contract SportsBet {
     enum BetStatus { Open, Paid }
 
     // indexing on a string causes issues with web3, so category has to be an int
-    event GameCreated(bytes32 id, string home, string away, uint16 indexed category, uint64 locktime);
+    event GameCreated(bytes32 indexed id, string home, string away, uint16 indexed category, uint64 locktime);
     event BidPlaced(bytes32 indexed game_id, BookType book, address bidder, uint amount, bool home, int32 line);
     event BetPlaced(bytes32 indexed game_id, BookType book, address home, address away, uint amount, int32 line);
 
